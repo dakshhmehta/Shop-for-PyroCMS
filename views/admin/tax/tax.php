58ea41c7cd9ad9899fb3d65bc73a4090eca2dee7
wollombiwombat <john@ikoniq.com.au>
2013-11-25 21:42:40 +1100

    Updates for Issue #55. Convert shop_lang - > lang on /views/admin/tax/*

--- views/admin/tax/tax.php
+++ views/admin/tax/tax.php
@@ -1,6 +1,6 @@
 
 <section class="title">
-	<h4><?php echo shop_lang('shop:tax:tax'); ?></h4>
+	<h4><?php echo lang('shop:tax:tax'); ?></h4>
 	<h4 style='float:right'>
 		<a href='admin/shop/tax/create/' class='tooltip-s img_icon_title img_create' title='<?php echo lang('shop:admin:new'); ?>'></a>
 	</h4>
@@ -12,17 +12,17 @@
 	<div class="content">
 		<?php if (empty($taxes)): ?>
 			<div class="no_data">
-			<p><?php echo shop_lang('shop:tax:description'); ?></p>
-			<?php echo shop_lang('shop:tax:no_data'); ?>
+			<p><?php echo lang('shop:tax:description'); ?></p>
+			<?php echo lang('shop:tax:no_data'); ?>
 			</div>
 		<?php else: ?>
 		<table>			
 			<thead>
 				<tr>
 					<th><input type="checkbox" name="action_to_all" value="" class="check-all" /></th>
-					<th><?php echo shop_lang('shop:tax:id'); ?></th>
-					<th><?php echo shop_lang('shop:tax:name'); ?></th>
-					<th><?php echo shop_lang('shop:tax:rate'); ?></th>
+					<th><?php echo lang('shop:tax:id'); ?></th>
+					<th><?php echo lang('shop:tax:name'); ?></th>
+					<th><?php echo lang('shop:tax:rate'); ?></th>
 					<th style="width: 120px"></th>
 				</tr>
 			</thead>
@@ -34,8 +34,8 @@
 						<td><?php echo $tax->name; ?></td>
 						<td><?php echo $tax->rate; ?> %</td>
 						<td>
-							<a title='<?php echo shop_lang('shop:tax:edit'); ?>' class="tooltip-s img_icon img_edit" href="<?php echo site_url('admin/shop/tax/edit/' . $tax->id); ?>"></a>
-							<a title= '<?php echo shop_lang('shop:tax:delete'); ?>' class="tooltip-s img_icon img_delete confirm" href="<?php echo site_url('admin/shop/tax/delete/' . $tax->id); ?>"></a>
+							<a title='<?php echo lang('shop:tax:edit'); ?>' class="tooltip-s img_icon img_edit" href="<?php echo site_url('admin/shop/tax/edit/' . $tax->id); ?>"></a>
+							<a title= '<?php echo lang('shop:tax:delete'); ?>' class="tooltip-s img_icon img_delete confirm" href="<?php echo site_url('admin/shop/tax/delete/' . $tax->id); ?>"></a>
 						</td>
 					</tr>
 				<?php endforeach; ?>
